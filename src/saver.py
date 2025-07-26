50f130863fd5fc17b2acbce43eda50f41acc44a6
Nadezhda Kornienko <service911@bk.ru>
2025-07-27 08:57:47 +1000

    feat: add CSVSaver class to saver.py

--- src/saver.py
+++ src/saver.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import os
 from abc import ABC, abstractmethod
@@ -49,3 +50,36 @@ class JSONSaver(Saver):
         with open(self.__filename, "w", encoding="UTF-8") as f:
             json.dump([v.as_dict() for v in vacancies], f, ensure_ascii=False, indent=4)
 
+
+class CSVSaver(Saver):
+    def __init__(self, filename: str) -> None:
+        self.__filename = data_folder + filename + ".csv"
+
+    def get_vacancies(self) -> list[Vacancy]:
+        if not os.path.exists(self.__filename):
+            return []
+        vacancies = []
+        with open(self.__filename, "r", encoding="UTF-8") as f:
+            data = csv.DictReader(f)
+            for row in data:
+                vacancy = Vacancy(**row)
+                vacancies.append(vacancy)
+            return vacancies
+
+    def add_vacancy(self, vacancy: Vacancy) -> None:
+        vacancies = self.get_vacancies()
+        if vacancy not in vacancies:
+            vacancies.append(vacancy)
+            self.save_to_file(vacancies)
+
+    def remove_vacancy(self, vacancy: Vacancy) -> None:
+        vacancies = self.get_vacancies()
+        vacancies = list(filter(lambda v: v != vacancy, vacancies))
+        self.save_to_file(vacancies)
+
+    def save_to_file(self, vacancies: list[Vacancy]) -> None:
+        with open(self.__filename, "w", newline="", encoding="UTF-8") as f:
+            fields = csv.DictWriter(f, fieldnames=vacancies[0].as_dict().keys())
+            fields.writeheader()
+            for v in vacancies:
+                fields.writerow(v.as_dict())
