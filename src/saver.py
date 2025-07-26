fd18437b911f6cc8680ded7b8b105c2fdbc18091
Nadezhda Kornienko <service911@bk.ru>
2025-07-27 08:57:20 +1000

    feat: add JSONSaver class to saver.py

--- src/saver.py
+++ src/saver.py
@@ -1,3 +1,4 @@
+import json
 import os
 from abc import ABC, abstractmethod
 
@@ -19,4 +20,32 @@ class Saver(ABC):
     @abstractmethod
     def get_vacancies(self) -> list[Vacancy]:
         pass
+
+
+class JSONSaver(Saver):
+    def __init__(self, filename: str) -> None:
+        self.__filename = data_folder + filename + ".json"
+
+    def get_vacancies(self) -> list[Vacancy]:
+        if not os.path.exists(self.__filename):
+            return []
+        else:
+            with open(self.__filename, "r", encoding="UTF-8") as f:
+                data = json.load(f)
+                return [Vacancy(**item) for item in data]
+
+    def add_vacancy(self, vacancy: Vacancy) -> None:
+        vacancies = self.get_vacancies()
+        if vacancy not in vacancies:
+            vacancies.append(vacancy)
+        self.save_to_file(vacancies)
+
+    def remove_vacancy(self, vacancy: Vacancy) -> None:
+        vacancies = self.get_vacancies()
+        vacancies = list(filter(lambda v: v != vacancy, vacancies))
+        self.save_to_file(vacancies)
+
+    def save_to_file(self, vacancies: list[Vacancy]) -> None:
+        with open(self.__filename, "w", encoding="UTF-8") as f:
+            json.dump([v.as_dict() for v in vacancies], f, ensure_ascii=False, indent=4)
 
