9234451ce3aeac831ea180ed803ccdf47f0fd108
Nadezhda Kornienko <service911@bk.ru>
2025-07-27 08:50:42 +1000

    feat: add HeadHunterApi class to api.py

--- src/api.py
+++ src/api.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+import requests
+
+from src.vacancy import Vacancy
+
 
 class BaseApi(ABC):
     @abstractmethod
@@ -11,3 +15,25 @@ class BaseApi(ABC):
     def get_data(self, keyword: str) -> Any:
         pass
 
+
+class HeadHunterApi(BaseApi):
+    def __init__(self) -> None:
+        self.url = "https://api.hh.ru/vacancies"
+        self.params = {
+            "text": "",
+            "area": 113,
+            "per_page": 10,
+            "only_with_salary": True,  # Поиск по вакансиям, в которых указана зарплата.
+        }
+        self.headers = {"User-Agent": "HH-User-Agent"}
+        self.vacancies: list[Vacancy] = []
+        super().__init__()
+
+    def get_data(self, keyword: str) -> Any:
+        self.params["text"] = keyword
+        response = requests.get(self.url, params=self.params, headers=self.headers)
+        if response.status_code != 200:
+            print(response.status_code)
+            raise ConnectionError("Ошибка подключения")
+        data = response.json()
+        return data.get("items", [])
